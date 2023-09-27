11512ced88c238fea7df3d199f2b899616c9e7b9
Liamhewitt05 <112162777+Liamhewitt05@users.noreply.github.com>
2023-09-27 14:42:52 +0200

    :fire: :fish:

--- MinFlaskApp/utils.py
+++ MinFlaskApp/utils.py
@@ -8,6 +8,6 @@ def auth_required(f):
         auth = request.authorization
         if auth and auth.username == current_app.config["SITE_USER"] and auth.password == current_app.config["SITE_PASS"]:
             return f(*args, **kwargs)
-        return make_response("<h1>Access denied!<h1>", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+        return make_response("<h1>Access denied!!!!!<h1>", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
     return decorated
